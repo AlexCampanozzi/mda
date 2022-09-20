07b6b7996cc390511482281792d2fe1d14cebc20
victoriapc <pitv4001@usherbrooke.ca>
2022-09-19 20:38:16 -0400

    added half opacity function

--- Explorus/Model/GameObjects/Rigidbodies/Animator.cs
+++ Explorus/Model/GameObjects/Rigidbodies/Animator.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Drawing;
+using System.Drawing.Imaging;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -21,6 +22,19 @@ namespace Explorus.Model.GameObjects.Rigidbodies
         public virtual Image Animate(int progress)
         {
             return image;
+        }
+        public Image halfOpacity(Image img)
+        {
+            Bitmap bitmap = new Bitmap(img.Width, img.Height);
+            Graphics graphics = Graphics.FromImage(bitmap);
+            ColorMatrix matrix = new ColorMatrix();
+            matrix.Matrix33 = 0.5f;
+            ImageAttributes imgAtt = new ImageAttributes();
+            imgAtt.SetColorMatrix(matrix, ColorMatrixFlag.Default, ColorAdjustType.Bitmap);
+            graphics.DrawImage(img, new Rectangle(0, 0, img.Width, img.Height), 0, 0, img.Width, img.Height, GraphicsUnit.Pixel, imgAtt);
+            graphics.Dispose();
+
+            return (bitmap.Clone(new Rectangle(0, 0, img.Width, img.Height), new Bitmap("./Resources/TilesSheet.png").PixelFormat));
         }
     }
 }
