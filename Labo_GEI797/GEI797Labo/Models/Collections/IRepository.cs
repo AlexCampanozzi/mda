75d20cbe703bef63fae6d0a56e1bb79b44000103
Alex Chorel-Campanozzi <choa3403@usherbrooke.ca>
2022-10-12 13:58:29 -0400

    lab3

---
+++ Labo_GEI797/GEI797Labo/Models/Collections/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GEI797Labo.Models.Collections
+{
+    public interface IRepository<T>
+    {
+        T GetItem(int index);
+        List<T> getAll();
+        void Insert(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+    }
+}
---
+++ Labo_GEI797/GEI797Labo/Models/Collections/ShapeRepository.cs
@@ -0,0 +1,48 @@
+﻿using GEI797Labo.Models.Definitions;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GEI797Labo.Models.Collections
+{
+    internal class ShapeRepository : IRepository<Shape>
+    {
+        private readonly List<Shape> oShapes;
+        private bool disposed = false;
+
+        public ShapeRepository() 
+        {
+
+        }
+
+        public List<Shape> FindById(params object[] values)
+        {
+            for i in 
+            return oShapes[id];
+        }
+
+        public IEnumerable FindAll()
+        {
+            return null;
+        }
+
+        public void Insert(Shape entity)
+        {
+            oShapes.Add(entity);
+        }
+
+        public void Update(Shape entity)
+        {
+            
+        }
+
+        public void Delete(Shape entity)
+        {
+            oShapes.Remove(entity);
+        }
+
+    }
+}
