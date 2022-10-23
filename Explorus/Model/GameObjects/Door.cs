826a167fe826a54b6962a5c21cc3e3ca7eec73b4
EtienneDesbiens <etiennedesbiens@hotmail.com>
2022-10-23 02:28:03 -0400

    no message

--- Explorus/Model/GameObjects/Door.cs
+++ Explorus/Model/GameObjects/Door.cs
@@ -39,20 +39,9 @@ namespace Explorus.Model
                     imgAtt.SetColorMatrix(matrix, ColorMatrixFlag.Default, ColorAdjustType.Bitmap);
                     graphics.DrawImage(img, new Rectangle(0, 0, img.Width, img.Height), 0, 0, img.Width, img.Height, GraphicsUnit.Pixel, imgAtt);
                     graphics.Dispose();
-            Bitmap bitmap = new Bitmap(img.Width, img.Height);
-            Graphics graphics = Graphics.FromImage(bitmap);
-            ColorMatrix matrix = new ColorMatrix();
-            matrix.Matrix33 = 0.5f;
-            ImageAttributes imgAtt = new ImageAttributes();
-            imgAtt.SetColorMatrix(matrix, ColorMatrixFlag.Default, ColorAdjustType.Bitmap);
-            graphics.DrawImage(img, new Rectangle(0, 0, img.Width, img.Height), 0, 0, img.Width, img.Height, GraphicsUnit.Pixel, imgAtt);
-            graphics.Dispose();
-            
-                    {
-                        SetImage(bitmap.Clone(new Rectangle(0, 0, 96, 96), new Bitmap("./Resources/TilesSheet.png").PixelFormat));
-            SetImage(bitmap.Clone(new Rectangle(0, 0, 96, 96), new Bitmap("./Resources/TilesSheet.png").PixelFormat));
-            }
-            catch { }
+                        
+                    SetImage(bitmap.Clone(new Rectangle(0, 0, 96, 96), new Bitmap("./Resources/TilesSheet.png").PixelFormat));
+                }
             }
             catch
             {
